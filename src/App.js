60f2f2602a09e1dc000634ed60e10c42a481df6c
Clément MGT <cmt94.dev@gmail.com>
2023-01-28 10:21:43 +0100

    init Calendar page and FullCalendar handle

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import ProfileForm from "./scenes/profile-form";
+import Calendar from "./scenes/calendar";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar";
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -38,12 +38,12 @@ const App = () => {
                 <Route path={"/contacts"} element={<Contacts />} />
                 <Route path={"/invoices"} element={<Invoices />} />
                 <Route path={"/form"} element={<ProfileForm />} />
+                <Route path={"/calendar"} element={<Calendar />} />
                 {/* <Route path={"/bar"} element={<Bar />} /> */}
                 {/* <Route path={"/pie"} element={<Pie />} /> */}
                 {/* <Route path={"/line"} element={<Line />} /> */}
                 {/* <Route path={"/faq"} element={<FAQ />} /> */}
                 {/* <Route path={"/geography"} element={<Geography />} /> */}
-                <Route path={"/calendar"} element={<Calendar />} />
               </Routes>
             </main>
           </div>
--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
 import FullCalendar from "@fullcalendar/react";
 import { formatDate } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -16,11 +19,100 @@ import {
   useTheme,
 } from "@mui/material";
 
-import Header from "../../components/Header";
-import { tokens } from "../../theme";
-
 const Calendar = () => {
-  return <div></div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const [currentEvents, setCurrentEvents] = React.useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}'`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
+
+  return (
+    <Box margin="20px">
+      <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+      <Box display="flex" justifyContent="space-between">
+        {/* CALENDAR SIDEBAR - START */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {
+                        (formatDate(event.start),
+                        {
+                          year: "numeric",
+                          month: "short",
+                          day: "numeric",
+                        })
+                      }
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        {/* CALENDAR SIDEBAR - END */}
+        {/* CALENDAR - START */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev, next, today",
+              center: "dayGridMonth, timeGridWeek, timeGridDay, listMonth",
+            }}
+          />
+        </Box>
+        {/* CALENDAR - END */}
+      </Box>
+    </Box>
+  );
 };
 
 export default Calendar;
